cbb9ccfea8b67d8777ddf5d76e50f176e97636d8
Jayakiran Akurathi <Jayakiran.Akurathi@gmail.com>
2016-04-16 04:49:40 +0530

    A couple of more bb constants for Bishops

--- src/consts.rs
+++ src/consts.rs
@@ -25,7 +25,11 @@ pub const W_CASTLE: u8 = WK_CASTLE | WQ_CASTLE;
 pub const B_CASTLE: u8 = BK_CASTLE | BQ_CASTLE;
 
 // BitBoard Constants
-pub const FIRST_ROW: u64 = 255u64;
-pub const A_FILE: u64 = 72340172838076673u64;
-pub const EDGES: u64 = 18411139144890810879u64;
-pub const NOT_EDGES: u64 = 35604928818740736u64;
+pub const FIRST_ROW: u64 = 0x00000000000000FFu64;
+pub const A_FILE: u64 = 0x0101010101010101u64;
+pub const A1_H8: u64 = 0x8040201008040201u64;
+pub const A8_H1: u64 = 0x0102040810204080u64;
+pub const LRT: u64 = 0x0080C0E0F0F8FCFE; // i > j
+pub const ULT: u64 = !LRT; // i <= j // includes A1_H8
+pub const EDGES: u64 = 0xFF818181818181FFu64;
+pub const NOT_EDGES: u64 = !EDGES;
