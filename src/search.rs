67edc2efe9fca840e8b4a8d2a3d42004b41218ec
Jayakiran Akurathi <Jayakiran.Akurathi@gmail.com>
2016-09-17 21:27:50 +0530

    remove redundant variable

--- src/search.rs
+++ src/search.rs
@@ -12,22 +12,20 @@ pub fn negamax( state: &mut State, depth: usize, mut alpha: i32, beta: i32 ) -> 
     if depth == 0 {
         static_eval( state )
     } else {
-        let mut best_eval = -MATE_VALUE;
-        let mut eval;
+        let mut eval = -MATE_VALUE;
 
         let ( legal_moves, _status ) = state.node_info();
         let irs = state.ir_state();
 
         for mv in &legal_moves {
             state.make( mv );
-            eval = -negamax( state, depth - 1, -beta, -alpha );
+            eval = cmp::max( eval, -negamax( state, depth - 1, -beta, -alpha ) );
             state.unmake( mv, &irs );
 
-            best_eval = cmp::max( best_eval, eval );
             alpha = cmp::max( alpha, eval );
             if beta <= alpha { break; } // Can also fail hard if you want
         }
 
-        best_eval
+        eval
     }
 }
