5b7838cb58c0e00ff88ba030c743f135743e470d
Jayakiran Akurathi <Jayakiran.Akurathi@gmail.com>
2016-09-20 01:18:20 +0530

    Fix comments on PSTs

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -33,27 +33,17 @@ pub const MG_PHASE: i32 = 128;
 pub const DRAW_VALUE: i32 = 0;
 pub const MATE_VALUE: i32 = 32000;
 
-// Square index map for PSTs
-pub const SQUARE_MAP: [ usize; 64 ] = [ 56, 57, 58, 59, 60, 61, 62, 63,
-                                        48, 49, 50, 51, 52, 53, 54, 55,
-                                        40, 41, 42, 43, 44, 45, 46, 47,
-                                        32, 33, 34, 35, 36, 37, 38, 39,
-                                        24, 25, 26, 27, 28, 29, 30, 31,
-                                        16, 17, 18, 19, 20, 21, 22, 23,
-                                         8,  9, 10, 11, 12, 13, 14, 15,
-                                         0,  1,  2,  3,  4,  5,  6,  7 ];
-
- // Piece square tables
- // https://chessprogramming.wikispaces.com/Simplified+evaluation+function
- // NOTE: The columns are inverted - they run from h to a. For simplicity, you can choose to keep the values symmetric around the vertical axis.... or not.
- pub const PAWN_PST: [ i32; 64 ] = [  0,  0,  0,  0,  0,  0,  0,  0,
-                                     50, 50, 50, 50, 50, 50, 50, 50,
-                                     10, 10, 20, 30, 30, 20, 10, 10,
-                                      5,  5, 10, 25, 25, 10,  5,  5,
-                                      0,  0,  0, 20, 20,  0,  0,  0,
-                                      5, -5,-10,  0,  0,-10, -5,  5,
-                                      5, 10, 10,-20,-20, 10, 10,  5,
-                                      0,  0,  0,  0,  0,  0,  0,  0 ];
+// Piece square tables
+// https://chessprogramming.wikispaces.com/Simplified+evaluation+function
+// NOTE: The columns are inverted for both White [ h -> a ] and Black [ a -> h ]. KEEP THE PST SYMMETRIC AROUND THE VERTICAL AXIS!
+pub const PAWN_PST: [ i32; 64 ] = [  0,  0,  0,  0,  0,  0,  0,  0,
+                                    50, 50, 50, 50, 50, 50, 50, 50,
+                                    10, 10, 20, 30, 30, 20, 10, 10,
+                                     5,  5, 10, 25, 25, 10,  5,  5,
+                                     0,  0,  0, 20, 20,  0,  0,  0,
+                                     5, -5,-10,  0,  0,-10, -5,  5,
+                                     5, 10, 10,-20,-20, 10, 10,  5,
+                                     0,  0,  0,  0,  0,  0,  0,  0 ];
 
 pub const KNIGHT_PST: [ i32; 64 ] = [ -50,-40,-30,-30,-30,-30,-40,-50,
                                       -40,-20,  0,  0,  0,  0,-20,-40,
